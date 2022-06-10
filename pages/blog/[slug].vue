9c021440e763810b5baf8178a65f8be936368175
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-11 00:03:23 +0900

    feat: ogp

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,9 +1,27 @@
 <template lang="pug">
-//- q-page(padding)
-content-doc
+q-page(padding)
+  content-doc
 
 </template>
 
 <script setup lang="ts">
-const { data } = await useAsyncData('hello', () => queryContent('/hello').findOne())
+const { path } = useRoute()
+const { data } = await useAsyncData('blog', () => {
+  const slug = path.split('/').pop()
+  return queryContent('blog').where({ slug: slug }).findOne()
+})
+
+console.log(data.value)
+
+useHead({
+  title: data.value.title,
+  meta: [
+    {
+      name: 'description',
+      content: data.value.description,
+    },
+    { hid: 'og:image', name: 'og:image', property: 'og:image', content: data.value.image },
+  ]
+})
+
 </script>
