f52d80bb9c996fde446de33a9903020732ed6458
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-10 23:23:36 +0900

    feat: to spa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
+  target: "server",
 
   buildModules: ["nuxt-vite"],
 
