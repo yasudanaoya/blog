6de23ef7ee699b050abc117ccb4ecdffcba862b9
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-11 12:35:30 +0900

    Revert "evert "fix: rm conf""
    This reverts commit 7c27e5c0ffc28e7459b14444f68ba88a9788442f.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
-  target: "static",
-
   buildModules: ["nuxt-vite"],
 
   css: ["quasar/dist/quasar.prod.css"],
