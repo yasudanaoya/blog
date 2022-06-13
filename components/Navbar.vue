86f4e69e52bf8515810ef5f8c905aefa2f9879dc
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-13 23:05:49 +0900

    feat: tab menu

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,48 +1,46 @@
 <template lang="pug">
-nav.sticky.top-0.backdrop-blur
-  div(class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center")
-    div(class="flex items-center justify-between")
-      div
-        nuxt-link(
-          class="text-xl font-dotgothic transition-colors duration-200 transform lg:text-3xl hover:text-sky-300"
-          to="/"
-        ) waino BLOG desu
-      div(class="flex md:hidden")
-        button(
-          type="button"
-          class="hover:text-sky-300"
-          aria-label="toggle menu"
-          @click="onToggle"
+nav
+  .navbar.sticky.top-0.backdrop-blur
+    .navbar-start
+
+    .navbar-center
+      div(class="container py-4 md:flex md:justify-between md:items-center")
+        div(class="flex items-center justify-between")
+          div
+            nuxt-link(
+              class="text-xl font-dotgothic transition-colors duration-200 transform lg:text-3xl hover:text-sky-300"
+              to="/"
+            ) waino BLOG desu
+
+    .navbar-end
+      div(class="block md:hidden dropdown dropdown-end")
+        label(
+          class="btn btn-ghost rounded-btn hover:text-sky-300"
+          tabindex="0"
         )
           font-awesome-icon(icon="bars")
-    div(
-      class="items-center md:flex"
-      :class="open ? 'block' : 'hidden'"
-    )
-      div(class="flex flex-col md:flex-row md:mx-6")
+        ul(
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-300 rounded-box"
+        )
+          li
+            nuxt-link(
+              v-for="tab in tabs"
+              :key="tab.path"
+              :to="tab.path"
+              :class="{ 'text-sky-300': isActive(tab.path) }"
+              class="my-1 text-sm font-medium transition-colors duration-200 transform hover:text-sky-300 md:mx-4 md:my-0"
+            )
+              | {{ tab.name }}
+
+      div(class="tabs hidden md:block")
         nuxt-link(
           v-for="tab in tabs"
           :key="tab.path"
           :to="tab.path"
-          :class="{ 'text-sky-300': isActive(tab.path) }"
-          class="my-1 text-sm font-medium transition-colors duration-200 transform hover:text-sky-300 md:mx-4 md:my-0"
-        )
-          | {{ tab.name }}
-
-  //- .navbar-center
-  //-   nuxt-link(
-  //-     class="text-xl font-dotgothic hover:text-sky-300"
-  //-   )
-  //-     | waino BLOG desu
-
-  //- .navbar-end
-    .tabs
-      nuxt-link.tab(
-        v-for="tab in tabs"
-        :to="tab.path"
-        :class="{ 'tab-active': isActive(tab.path) }"
-      )
-        | {{ tab.name }}
+          :class="{ 'tab-active': isActive(tab.path) }"
+          class="tab"
+        ) {{ tab.name }}
 </template>
 
 <script setup lang="ts">
