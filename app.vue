5131f61003aeaf8b8a54c6466d4ad060a427ad1c
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-14 00:55:12 +0900

    feat: head

--- app.vue
+++ app.vue
@@ -2,3 +2,42 @@
 nuxt-layout
   nuxt-page
 </template>
+
+<script setup lang="ts">
+const baseUrl = "yasudanaoya.com";
+const title = "yasudanaoya's Site";
+const description = "ブログを投稿したり、写真を投稿したりします。";
+const image = "https://avatars.githubusercontent.com/u/43776161?v=4"
+
+useHead({
+  title: title,
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      hid: "description",
+      name: "description",
+      content: "ブログを投稿したり、写真を投稿したりします。",
+    },
+    {
+      hid: "og:site_name",
+      property: "og:site_name",
+      content: title,
+    },
+    { hid: "og:type", property: "og:type", content: "website" },
+    { hid: "og:url", property: "og:url", content: baseUrl },
+    { hid: "og:title", property: "og:title", content: title },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: description,
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: image,
+    },
+    { name: "twitter:card", content: "summary" },
+  ],
+})
+</script>
