e389df159e3ca8c3faf661ebc09f99bc76272655
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-13 22:23:16 +0900

    feat: new nabvar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,41 @@
 <template lang="pug">
-nav.navbar.sticky.top-0.backdrop-blur
-  .navbar-start
-  .navbar-center
-    nuxt-link(
-      to="/"
-      class="text-xl font-dotgothic hover:text-sky-300"
+nav.sticky.top-0.backdrop-blur
+  div(class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center")
+    div(class="flex items-center justify-between")
+      div
+        nuxt-link(
+          class="text-xl font-dotgothic transition-colors duration-200 transform lg:text-3xl hover:text-sky-300"
+          to="/"
+        ) waino BLOG desu
+      div(class="flex md:hidden")
+        button(
+          type="button"
+          class="hover:text-sky-300"
+          aria-label="toggle menu"
+          @click="onToggle"
+        )
+          font-awesome-icon(icon="bars")
+    div(
+      class="items-center md:flex"
+      :class="open ? 'block' : 'hidden'"
     )
-      | waino BLOG desu
+      div(class="flex flex-col md:flex-row md:mx-6")
+        nuxt-link(
+          v-for="tab in tabs"
+          :key="tab.path"
+          :to="tab.path"
+          :class="{ 'text-sky-300': isActive(tab.path) }"
+          class="my-1 text-sm font-medium transition-colors duration-200 transform hover:text-sky-300 md:mx-4 md:my-0"
+        )
+          | {{ tab.name }}
 
-  .navbar-end
+  //- .navbar-center
+  //-   nuxt-link(
+  //-     class="text-xl font-dotgothic hover:text-sky-300"
+  //-   )
+  //-     | waino BLOG desu
+
+  //- .navbar-end
     .tabs
       nuxt-link.tab(
         v-for="tab in tabs"
@@ -41,5 +68,10 @@ const isActive = (path: string) => {
     return route.path === '/'
   }
   return route.path.match(new RegExp(path))
+}
+
+const open = ref(false)
+const onToggle = () => {
+  open.value = !open.value
 }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div.flex.flex-col.h-screen.justify-between
+div.flex.flex-col.min-h-screen.h-full.justify-between
   navbar
   slot
   app-footer
