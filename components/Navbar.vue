fb6e984e62d17570c33225571ee6710760278094
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-14 22:13:27 +0900

    feat: use drawer

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,58 +1,49 @@
 <template lang="pug">
-nav
-  .navbar.sticky.top-0.backdrop-blur
-    .navbar-start
+.drawer.drawer-end
+  input#nav-drawer.drawer-toggle(type='checkbox')
+  .drawer-content.flex.flex-col
+    .w-full.navbar.bg-base-300
+      .navbar-start
 
-    .navbar-center
-      div(class="container py-4 md:flex md:justify-between md:items-center")
-        div(class="flex items-center justify-between")
-          div
+      .navbar-center
+        div(class="container py-4 md:flex md:justify-between md:items-center")
+          div(class="flex items-center justify-between")
             nuxt-link(
               class="text-xl font-dotgothic transition-colors duration-200 transform lg:text-3xl hover:text-sky-300"
               to="/"
             ) waino BLOG desu
 
-    .navbar-end
-      div(class="block md:hidden")
-        Menu
-          div
-            menu-button(class="btn btn-ghost rounded-btn hover:text-sky-300")
-              font-awesome-icon(icon="bars")
-          transition(
-            enter-active-class="transition duration-100 ease-out"
-              enter-from-class="transform scale-95 opacity-0"
-              enter-to-class="transform scale-100 opacity-100"
-            leave-active-class="transition duration-75 ease-in"
-              leave-from-class="transform scale-100 opacity-100"
-              leave-to-class="transform scale-95 opacity-0"
-          )
-            menu-items(
-              class="absolute right-0 p-2 shadow bg-base-300 rounded-box ring-opacity-5 focus:outline-none"
-            )
-              .px-1.py-1
-                menu-item(
-                  v-for="tab in tabs"
-                  :key="tab.path"
-                  v-slot="{ active }"
-                )
-                  nuxt-link(
-                    :class="{ 'text-sky-300': active }"
-                    class="my-1 text-sm font-medium group flex w-full px-2 transition-colors duration-200 transform md:mx-4 md:my-0"
-                    :to="tab.path"
-                  ) {{ tab.name }}
+      .navbar-end
+        div(class="flex-none lg:hidden")
+          label.btn.btn-square.btn-ghost(for='nav-drawer' ref='btn')
+            font-awesome-icon(icon="bars")
+        div(class="flex-none hidden lg:block")
+          div(class="tabs flex-none hidden lg:block")
+            nuxt-link(
+              v-for="tab in tabs"
+              :key="tab.path"
+              :to="tab.path"
+              :class="{ 'tab-active': isActive(tab.path) }"
+              class="tab"
+            ) {{ tab.name }}
+    slot
 
-      div(class="tabs hidden md:block")
+  .drawer-side
+    label.drawer-overlay(for='nav-drawer')
+    ul.menu.p-4.overflow-y-auto.w-80.bg-base-100
+      li(
+        v-for="tab in tabs"
+        :key="tab.path"
+      )
         nuxt-link(
-          v-for="tab in tabs"
-          :key="tab.path"
+          @click.native="click"
           :to="tab.path"
-          :class="{ 'tab-active': isActive(tab.path) }"
-          class="tab"
-        ) {{ tab.name }}
+        )
+          | {{ tab.name }}
+
 </template>
 
 <script setup lang="ts">
-import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
 const tabs = [
   {
     name: 'Home',
@@ -68,17 +59,17 @@ const tabs = [
   },
 ]
 
-const route = useRoute()
 
+const btn = ref(null)
+const click = () => {
+  btn.value.click()
+}
+
+const route = useRoute()
 const isActive = (path: string) => {
   if (path === '/') {
     return route.path === '/'
   }
   return route.path.match(new RegExp(path))
-}
-
-const open = ref(false)
-const onToggle = () => {
-  open.value = !open.value
 }
 </script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div.flex.flex-col.min-h-screen.h-full.justify-between
   navbar
-  slot
-  app-footer
+    slot
+    app-footer
 </template>
