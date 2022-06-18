9777fd4d763e9fc26a9d3e4f1b3ae1b4b4819e86
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2022-06-18 18:52:49 +0900

    feat: add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div(class="flex flex-col justify-center items-center content-center")
+
+  div(class="mockup-code w-1/2")
+    pre(data-prefix="$")
+      code {{ `ls ~/${requestValue}` }}
+    pre.bg-error.text-error-content(data-prefix=">")
+      code {{ `ls: ~/${requestValue}: No such file or directory` }}
+</template>
+
+<script setup lang="ts">
+const { path } = useRoute()
+const requestValue = path.replace('/', '')
+</script>
