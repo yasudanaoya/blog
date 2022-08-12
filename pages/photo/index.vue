482ddf5925641a7430442a0dd8dcf0803a136849
yanskun <43776161+yanskun@users.noreply.github.com>
2022-08-12 15:52:28 +0900

    feat: add photo

--- pages/photo/index.vue
+++ pages/photo/index.vue
@@ -1,22 +1,12 @@
 <template lang="pug">
 .container.mx-auto.px-4.py-5
-  //- div(
-  //-   class="mx-auto grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-3"
-  //- )
-    photo-card(
-      v-for="img in images"
-      :src="img.src"
-      :desc="img.desc"
-    )
-
-
-  .carousel.rounded-box(
-    :style="{ height: '37rem' }"
+  div(
+    class="columns-2 md:columns-3 lg:columns-4"
   )
-    .carousel-item(
-      v-for="img in images"
+    img.mb-4(
+      v-for="img in imgs"
+      :src="img"
     )
-      img(:src="img.src")
 </template>
 
 <script setup lang="ts">
@@ -25,26 +15,13 @@ useHead({
 })
 
 // TODO: supabase storage から取得
-const images = [
-  {
-    src: "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/79659325-0B00-4321-A27A-19D44C9108CF_1_105_c.jpeg",
-    desc: 'カメラを買って嬉しいので調布飛行場で飛行機を撮った'
-  },
-  {
-    src: "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/36A88942-9029-4C22-A780-15C8D76BCB4C_1_105_c.jpeg",
-    desc: 'スノボに GitHub のステッカーを貼った'
-  },
-  {
-    src: "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/FC97D139-1CA7-46B4-9E63-797ACE4A45EE_1_105_c.jpeg",
-    desc: '春の終わりに御苑で'
-  },
-  {
-    src: "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/954E28BD-3B91-4733-BEF7-21FD0450FE5F_1_105_c.jpeg",
-    desc: '静岡旅行 薩埵峠からの景色',
-  },
-  {
-    src: "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/D521B25F-7D18-4909-BFCC-7DEC787021EC_1_105_c.jpeg",
-    desc: '年末の京都 自粛もあってかなり静か'
-  }
+const imgs = [
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/79659325-0B00-4321-A27A-19D44C9108CF_1_105_c.jpeg",
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/36A88942-9029-4C22-A780-15C8D76BCB4C_1_105_c.jpeg",
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/FC97D139-1CA7-46B4-9E63-797ACE4A45EE_1_105_c.jpeg",
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/954E28BD-3B91-4733-BEF7-21FD0450FE5F_1_105_c.jpeg",
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/D521B25F-7D18-4909-BFCC-7DEC787021EC_1_105_c.jpeg",
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/18F12F7B-0ED7-4A52-9BB2-1765147BC82D_1_105_c.jpeg",
+  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/E9EE9CB9-A7E0-457A-A970-445296BC2E47_1_105_c.jpeg",
 ]
 </script>
