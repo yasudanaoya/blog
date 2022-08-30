49a3f905a48f36ee449e7752a8c3858842bbe700
yanskun <43776161+yanskun@users.noreply.github.com>
2022-08-30 12:45:41 +0900

    feat: add rss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,59 +21,16 @@ export default defineNuxtConfig({
     "@fortawesome/fontawesome-svg-core/styles.css",
   ],
 
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/supabase",
-    "@nuxt/content",
-    "@nuxtjs/feed",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@nuxt/content"],
 
   generate: {
     fallback: "404.html",
     routes: ["/", "/404.html"],
   },
 
-  feed: {
-    async create() {
-      const baseUrlArticles = "https://yanskun.com/blog";
-      const baseLinkFeedArticles = "/feed/blog";
-      const feedFormats = {
-        rss: { type: "rss2", file: "rss.xml" },
-        atom: { type: "atom1", file: "atom.xml" },
-        json: { type: "json1", file: "feed.json" },
-      };
-      const { $content } = require("@nuxt/content");
-      const createFeedArticles = async function (feed) {
-        feed.options = {
-          title: "My Blog",
-          description: "I write about technology",
-          link: baseUrlArticles,
-        };
-        const articles = await $content("blog").fetch();
-        articles.forEach((article) => {
-          const url = `${baseUrlArticles}/${article.slug}`;
-          feed.addItem({
-            title: article.title,
-            id: url,
-            link: url,
-            date: article.date,
-            description: article.description,
-            content: article.description,
-            author: "yanskun",
-          });
-        });
-      };
-      return Object.values(feedFormats).map(({ file, type }) => ({
-        path: `${baseLinkFeedArticles}/${file}`,
-        type: type,
-        create: createFeedArticles,
-      }));
-    },
-  },
-
   nitro: {
     prerender: {
-      routes: ["/sitemap.xml"],
+      routes: ["/sitemap.xml", "/rss.xml"],
     },
   },
 });
