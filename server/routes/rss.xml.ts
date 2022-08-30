c13321182c91a84a24f726c2f2e8c97f59e128c3
yanskun <43776161+yanskun@users.noreply.github.com>
2022-08-30 12:52:26 +0900

    chore(fix): rss ts

--- server/routes/rss.xml.ts
+++ server/routes/rss.xml.ts
@@ -1,27 +1,29 @@
 import { serverQueryContent } from "#content/server";
 import RSS from "rss";
 
-const feed = new RSS({
-  title: "yanskun's Blog",
-  site_url: "https://yanskun.com",
-  feed_url: `https://yanskun.com/rss.xml`,
-});
-
-const docs = await serverQueryContent(event)
-  .sort({ date: -1 })
-  .where({ _partial: false })
-  .find();
-const blogPosts = docs.filter((doc) => doc?._path?.includes("/blog"));
-
-for (const doc of blogPosts) {
-  feed.item({
-    title: doc.title ?? "-",
-    url: `https://yanskun.com${doc._path}`,
-    date: doc.date,
-    description: doc.description,
+export default defineEventHandler(async (event) => {
+  const feed = new RSS({
+    title: "yanskun's Blog",
+    site_url: "https://yanskun.com",
+    feed_url: `https://yanskun.com/rss.xml`,
   });
-}
 
-const feedString = feed.xml({ indent: true });
-event.res.setHeader("content-type", "text/xml");
-event.res.end(feedString);
+  const docs = await serverQueryContent(event)
+    .sort({ date: -1 })
+    .where({ _partial: false })
+    .find();
+
+  const blogPosts = docs.filter((doc) => doc?._path?.includes("/blog"));
+  for (const doc of blogPosts) {
+    feed.item({
+      title: doc.title ?? "-",
+      url: `https://yanskun.com${doc._path}`,
+      date: doc.date,
+      description: doc.description,
+    });
+  }
+
+  const feedString = feed.xml({ indent: true });
+  event.res.setHeader("content-type", "text/xml");
+  event.res.end(feedString);
+});
