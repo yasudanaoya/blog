233220e6d7f7ea260c9c2256eec6a1f146f1746c
yanskun <43776161+yanskun@users.noreply.github.com>
2022-09-14 00:08:31 +0900

    feat: update photo

--- pages/photo/index.vue
+++ pages/photo/index.vue
@@ -4,7 +4,7 @@
     class="columns-2 md:columns-3 lg:columns-4"
   )
     img.mb-4(
-      v-for="img in imgs"
+      v-for="img in imgPaths()"
       :src="img"
     )
 </template>
@@ -16,12 +16,22 @@ useHead({
 
 // TODO: supabase storage から取得
 const imgs = [
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/79659325-0B00-4321-A27A-19D44C9108CF_1_105_c.jpeg",
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/36A88942-9029-4C22-A780-15C8D76BCB4C_1_105_c.jpeg",
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/FC97D139-1CA7-46B4-9E63-797ACE4A45EE_1_105_c.jpeg",
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/954E28BD-3B91-4733-BEF7-21FD0450FE5F_1_105_c.jpeg",
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/D521B25F-7D18-4909-BFCC-7DEC787021EC_1_105_c.jpeg",
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/18F12F7B-0ED7-4A52-9BB2-1765147BC82D_1_105_c.jpeg",
-  "https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/E9EE9CB9-A7E0-457A-A970-445296BC2E47_1_105_c.jpeg",
+  "202205_shizuoka.jpeg",
+  "202205_shizuoka2.jpeg",
+  "202205_shizuoka3.jpeg",
+  "2020_kyoto.jpeg",
+  "2022_gyoen.jpeg",
+  "2022_chofu_ airfield.jpeg",
+  "snowborad.jpeg",
+  "2022_ sunset.jpeg",
+  "2022_Mt_fuji.jpeg",
+  "2022_Mt_fuji2.jpeg",
+  "2022_okinawa.jpeg",
+  "2022_solo_camp.jpeg",
 ]
+const imgPaths = () => {
+  return imgs.map((img) => {
+    return `https://icxboltnnvznqlpuzkyg.supabase.co/storage/v1/object/public/photos/${img}`
+  })
+}
 </script>
