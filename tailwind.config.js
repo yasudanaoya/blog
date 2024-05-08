8296092aeaa906a1f0961a45708ef90f21949c5e
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-05-08 16:53:29 +0900

    chore: update nuxt version

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
   plugins: [
     require("daisyui"),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
 
